67e42e1177856d77e6a0f2d8e87912aa86219297
melanyhowell <melany.r.howell@gmail.com>
2022-12-31 20:54:26 +0000

    add in the moves counter js and candom item picks from the items array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,49 +1,69 @@
-const moves = document.getElementById("movesCount");
+const moves = document.getElementById("moves-count");
 const timeValue = document.getElementById("time");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
-const gameContainer = document.querySelector("game-container");
+const gameContainer = document.querySelector(".game-container");
 const result = document.getElementById("result");
-const controls = document.querySelector("controls-container");
-
+const controls = document.querySelector(".controls-container");
 let cards;
 let interval;
 let firstCard = false;
 let secondCard = false;
-
-//item arrays
-
+//Items array
 const items = [
-    {name:"pig", image: "pig.svg"}, 
-    {name:"rooster", image: "rooster.svg"}, 
-    {name:"sheep", image: "sheep.svg"}, 
-    {name:"squirrel", image: "squirrel.svg"}, 
-    {name:"kangaroo", image: "kangaroo.svg"}, 
-    {name:"hippo", image: "hippo.svg"}, 
-    {name:"goat", image: "goat.svg"}, 
-    {name:"elephant", image: "elephant.svg"}, 
-    {name:"duck", image: "duck.svg"}, 
-    {name:"donkey", image: "donkey.svg"},
+  { name: "bee", image: "bee.png" },
+  { name: "crocodile", image: "crocodile.png" },
+  { name: "macaw", image: "macaw.png" },
+  { name: "gorilla", image: "gorilla.png" },
+  { name: "tiger", image: "tiger.png" },
+  { name: "monkey", image: "monkey.png" },
+  { name: "chameleon", image: "chameleon.png" },
+  { name: "piranha", image: "piranha.png" },
+  { name: "anaconda", image: "anaconda.png" },
+  { name: "sloth", image: "sloth.png" },
+  { name: "cockatoo", image: "cockatoo.png" },
+  { name: "toucan", image: "toucan.png" },
 ];
-
 //Initial Time
-
 let seconds = 0,
-    minutes = 0;
+  minutes = 0;
 //Initial moves and win count
 let movesCount = 0,
-    winCount = 0;
-
+  winCount = 0;
 //For timer
 const timeGenerator = () => {
-    seconds += 1;
-    //minutes logic
-    if (seconds >= 60) {
-      minutes += 1;
-      seconds = 0;
-    }
-    //format time before displaying
-    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
-    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+  seconds += 1;
+//minutes logic
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+//format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
   };
+
+//For calculating moves
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+
+//Pick random objects from the items array
+const generateRandom = (size = 4) => {
+  //temporary array
+  let tempArray = [...items];
+  //initialises cardValues array
+  let cardValues = [];
+  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  size = (size * size) / 2;
+  //Random object selection
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    //once selected remove the object from temp array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
